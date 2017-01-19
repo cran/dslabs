7f997133091d165d71792dd95b7151b5f7058c5e
Rafael A. Irizarry <rafa@jimmy.harvard.edu>
2017-01-19 10:17:45 +0000

    version 0.0.1

---
+++ R/murders.R
@@ -0,0 +1,20 @@
+#' US gun murders by state for 2010
+#' 
+#' Totals are from FBI reports. Population of each state for 2010.
+#'
+#'
+#' @docType data
+#'
+#' @usage data(murders)
+#'
+#' @format An object of class \code{"data.frame"}. 
+#'
+#' @keywords datasets
+#'
+#'
+#' @source \href{https://en.wikipedia.org/wiki/Murder_in_the_United_States_by_state}{Wikipedia}
+#'
+#' @examples
+#' data(murders)
+#' print(murders)
+"murders"
