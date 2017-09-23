2af01b3cb23a650519137d023835093a745bd526
Rafael A. Irizarry <rafa@jimmy.harvard.edu>
2017-09-23 19:24:23 +0000

    version 0.2.0

---
+++ R/divorce_margarine.R
@@ -0,0 +1,24 @@
+#' Divorce rate and margarine consumption data
+#' 
+#' Divorce rates in Maine and per capita consumption of margarine in US data
+#' 
+#' 
+#'
+#' \itemize{
+#' divorce_rate_maine. Divorce per 1000 in Maine.
+#' margarine_consumption_per_capita. US per capita consumption of margarine in pounds.
+#' year. Year.
+#' }
+#'
+#' @docType data
+#'
+#' @usage data(divorce_margarine)
+#'
+#' @format An object of class \code{"data.frame"}.
+#'
+#' @keywords datasets
+#'
+#' @source \url{http://www.tylervigen.com/spurious-correlations}{Spurious Correlations}
+#' data(divorce_margarine)
+#' with(divorce_margarine, plot(argarine_consumption_per_capita, divorce_rate_maine))
+"divorce_margarine"
