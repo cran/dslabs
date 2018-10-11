9b9491f31ccd2dc2a511e1359b9bfd594d3ff0b1
Rafael A. Irizarry <rafa@jimmy.harvard.edu>
2018-10-11 19:50:03 +0000

    version 0.5.0

--- R/polls_2008.R
+++ R/polls_2008.R
@@ -1,7 +1,7 @@
 #' Poll data for popular vote in 2008 presidential election
 #' 
 #' Data from different pollsters for the popular vote between Obama and McCain 
-#' in the 20008 presidential election.  
+#' in the 2008 presidential election.  
 #'  
 #'
 #' \itemize{
@@ -18,7 +18,7 @@
 #' @keywords datasets
 #'
 #'
-#' @source \href{https://web.archive.org/web/20160309010059/http://www.pollster.com/08USPresGEMvO-2.html}{https://web.archive.org/web/20160309010059/http://www.pollster.com/08USPresGEMvO-2.html}
+#' @source \href{https://web.archive.org/web/20161108190914/http://www.pollster.com/08USPresGEMvO-2.html}{https://web.archive.org/web/20161108190914/http://www.pollster.com/08USPresGEMvO-2.html}
 #'
 #' @examples
 #' data(polls_2008)
